b29b33c92c9aca1a5e1ea4a6e1dd83571160f928
Husni Hamza <hgchamza91@gmail.com>
2023-07-13 14:17:41 +0200

    Initial commit

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
